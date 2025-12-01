efe09d4b4deb50bca6b78dbf61292ccd6973f495
NopeUniversity <wmfuss01@louisville.edu>
2025-11-30 21:45:20 -0500

    Got all the data from steamdb into a list of lists with a game class. made it into a function to clean up the main function.

--- GroupProject_Main.py
+++ GroupProject_Main.py
@@ -10,6 +10,7 @@ def fileRead(folderPathName):
         with open(fileName, 'r', encoding='utf-8', errors='ignore') as f:
             text = f.read()
             engineFileList.append(text)
+            #engineFileList.append("NEXT SET OF TEXT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n")
             print(fileName)
             print(len(text))
 
@@ -18,6 +19,7 @@ def fileRead(folderPathName):
         with open(fileName, 'r', encoding='utf-8', errors='ignore') as f:
             text = f.read()
             engineFileList.append(text)
+            #engineFileList.append("NEXT SET OF TEXT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\n")
             print(fileName)
             print(len(text))
 
@@ -28,10 +30,7 @@ def fileRead(folderPathName):
         with open(os.path.join(os.getcwd(), filename), 'r') as f:  # open in readonly mode
             ...
 """
-
-if __name__ == '__main__':
-    folderPath = input("Please input the folder path: ")
-    engineFileList = fileRead(folderPath)
+def htmlToList(engineFileList): #takes in a list of the read files with each entry of the list being an entire html text document.
     engineList = []  # contains a list of all engines and the names of the titles
     tempList = []  # used in the loop to make a list for each engine. first item of the list will be the engine name. then each game name and app id will have an entry
     for lineString in engineFileList:
@@ -41,10 +40,9 @@ if __name__ == '__main__':
         # print(titleStart, titleEnd)
         tempList.append(lineString[titleStart:titleEnd])
         tempPosition = titleEnd
-        # print(tempList)
+        #print(tempList)
         # print(len(lineString))
-        while tempPosition < len(
-                lineString):  # this loop finds appIDs, names, price, rating score, and top player count of the games in that order
+        while tempPosition < len(lineString):  # this loop finds appIDs, names, price, rating score, and top player count of the games in that order
             if lineString.find('<a class="b" href="', tempPosition) == -1:
                 break
             # print("I GOT PASSED THE BREAK CHECK")
@@ -52,15 +50,78 @@ if __name__ == '__main__':
             tempPosition = nameStart
             nameEnd = lineString.find('</a>', tempPosition)
             nameStart = nameStart + len('<a class="b" href="')
-            tempList.append(lineString[nameStart:nameEnd])
-            tempList[len(tempList) - 1] = tempList[len(tempList) - 1].replace('/app/',
-                                                                              '')  # cleaning up the string so its just the appID and name separated by >
-            tempList[len(tempList) - 1] = tempList[len(tempList) - 1].replace('/"', '')
+            tempName = lineString[nameStart:nameEnd]
+            #tempList.append(lineString[nameStart:nameEnd])
+            #print(tempList)
+            #break
+            #cleaning stuff up for the game class
+            tempName = tempName.replace('/app/', '')
+            tempName = tempName.replace('/"', '')
+            tempName = tempName.replace("&apos;", "'")
+            tempName = tempName.replace("&quot;", '"')
+            nameStart = tempName.find(">")
+            tempID = tempName[:nameStart] #the id for the game class
+            tempName = tempName[nameStart:] #the name for the game class
+            """
+            tempList[len(tempList) - 1] = tempList[len(tempList) - 1].replace('/app/', '')  # cleaning up the string so its just the appID and name separated by >
+            tempList[len(tempList) - 1] = tempList[len(tempList) - 1].replace('/"', '')"""
             tempPosition = nameEnd  # start of trying to get the extra data
+            dataStart = lineString.find('</td>', tempPosition)
+            tempPosition = dataStart
+            dataEnd = lineString.find('</tr>', tempPosition)
+            dataStart = lineString.find('<td data-sort="', tempPosition) #getting the position of the discount
+            tempPosition = dataStart
 
-            dataStart = lineString.find('<td>', tempPosition)
-            # print(lineString[nameStart:nameEnd])
-        engineList.append(tempList)
-    print(engineList)
+            costStart = lineString.find('<td data-sort="', tempPosition) #getting the position of the cost
+            costEnd = lineString.find('">', costStart)
+            tempCost = lineString[costStart:costEnd]
+            tempPosition = costEnd
+
+            ratingStart = lineString.find('<td data-sort="', tempPosition)
+            ratingEnd = lineString.find('">', ratingStart)
+            tempRating = lineString[ratingStart:ratingEnd]
+            tempPosition = ratingEnd
+
+            releaseStart = lineString.find('<td data-sort="', tempPosition)
+            releaseEnd = lineString.find('">', releaseStart)
+            tempRelease = lineString[releaseStart:releaseEnd]
+            tempPosition = releaseEnd
+
+            dataStart = lineString.find('<td data-sort="', tempPosition)  #position of following count
+            tempPosition = dataStart
+            dataStart = lineString.find('<td data-sort="', tempPosition) #position of online count
+            tempPosition = dataStart
+
+            topPlayerCountStart = lineString.find('<td data-sort="', tempPosition)
+            topPlayerCountEnd = lineString.find('">', topPlayerCountStart)
+            tempTopPlayerCount = lineString[topPlayerCountStart:topPlayerCountEnd]
+
+            tempGame = Game(tempID, tempName, tempCost, tempRating, tempRelease, tempTopPlayerCount)
+            tempList.append(tempGame)
+
+        engineList.append(tempList.copy())
+    return engineList
+
+class Game:
+    def __init__(self, id, title, cost, rating, releaseDate, topPlayerCount):
+        self.id = id
+        self.title = title
+        self.cost = cost
+        self.rating = rating
+        self.releaseDate = releaseDate
+        self.topPlayerCount = topPlayerCount
+
+
+if __name__ == '__main__':
+    folderPath = input("Please input the folder path: ")
+    engineFileList = fileRead(folderPath)
+
+    engineList = htmlToList(engineFileList)
+
+
+
+        #engineList[]
+        #print(engineList[0])
+    print(len(engineList))
 
     # print(fileRead(folderPath))
